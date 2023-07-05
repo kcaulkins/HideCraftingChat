fa1ce14479cc593d0bbe720caa26d8ae22a53972
kolefield <kc@klfld.net>
2023-07-05 14:30:42 -0400

    Initial commit

---
+++ HideCraftingChat.lua
@@ -0,0 +1,6 @@
+local filterFunc = function(self, event, msg, author, ...)
+    if msg:find("creates:") then
+        return true
+    end
+end
+ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT", filterFunc)
